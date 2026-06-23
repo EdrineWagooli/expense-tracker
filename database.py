40930153c3636c74ead1b2b703a943a4758a3dde
EdrineWagooli <73692155+EdrineWagooli@users.noreply.github.com>
2026-06-23 20:23:13 +0300

    Stage 2: database engine, session factory and connection manager"

--- database.py
+++ database.py
@@ -1,1 +1,19 @@
 # Sets up the connection/engine to SQLite, nothing about what data looks like or how it's queried
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from typing import Generator
+
+from models import Base
+
+
+engine = create_engine('sqlite:///expense_tracker.db', echo=True)
+SessionLocal = sessionmaker(bind=engine)
+Base.metadata.create_all(engine)
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
