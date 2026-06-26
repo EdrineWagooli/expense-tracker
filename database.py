21f4728b90a64e079a34c986f28ca0eb50fb14e4
EdrineWagooli <73692155+EdrineWagooli@users.noreply.github.com>
2026-06-26 03:06:59 +0300

    # Sets up the connection/engine to SQLite, nothing about what data looks like or how it's queried

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ from typing import Generator
 from models import Base
 
 
-engine = create_engine('sqlite:///expense_tracker.db', echo=True)
+engine = create_engine('sqlite:///expense_tracker.db', echo=False)
 SessionLocal = sessionmaker(bind=engine)
 Base.metadata.create_all(engine)
 
